13b9065bd3d25a40ce9c698e2f48de7c25ed3eb4
David Runger <davidjrunger@gmail.com>
2020-06-13 20:53:27 -0700

    Autocorrect rubocop violations in spec/active_actions_spec.rb

--- spec/active_actions_spec.rb
+++ spec/active_actions_spec.rb
@@ -1,9 +1,11 @@
-RSpec.describe ActiveActions do
-  it "has a version number" do
-    expect(ActiveActions::VERSION).not_to be nil
+# frozen_string_literal: true
+
+RSpec.describe(ActiveActions) do
+  it 'has a version number' do
+    expect(ActiveActions::VERSION).not_to(be(nil))
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  it 'does something useful' do
+    expect(false).to(eq(true))
   end
 end
