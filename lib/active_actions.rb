0fd9ecfb4bb1b97fe8b0c2394a849536175fe644
David Runger <davidjrunger@gmail.com>
2020-06-13 20:27:31 -0700

    Commit brand new gem `active_actions`
    `bundle gem active_actions`

---
+++ lib/active_actions.rb
@@ -0,0 +1,6 @@
+require "active_actions/version"
+
+module ActiveActions
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/active_actions/version.rb
@@ -0,0 +1,3 @@
+module ActiveActions
+  VERSION = "0.1.0"
+end
