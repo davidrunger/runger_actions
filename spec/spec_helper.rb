21999904fbe82c9f759d96cad1a57f8da1e69e10
David Runger <davidjrunger@gmail.com>
2020-06-18 12:25:10 -0700

    Add test coverage reporting (via `codecov` and `simplecov`)

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,14 @@
 # frozen_string_literal: true
 
+require 'simplecov'
+if ENV['CI'] == 'true'
+  require 'codecov'
+  SimpleCov.formatter = SimpleCov::Formatter::Codecov
+end
+SimpleCov.start do
+  add_filter(%r{\A/spec/})
+end
+
 require 'bundler/setup'
 
 require 'active_actions'
