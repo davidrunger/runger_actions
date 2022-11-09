38f6a1c8e8421a0aac2245e7638b5e8bdc99b226
David Runger <davidjrunger@gmail.com>
2022-11-08 23:47:47 -0600

    Explicitly define an `ActiveActions::Generators` module
    Without this, attempting to run the generator was generating an error
    about the constant `ActiveActions::Generators` being undefined.

--- lib/generators/active_actions/action/action_generator.rb
+++ lib/generators/active_actions/action/action_generator.rb
@@ -2,6 +2,8 @@
 
 require 'rails/generators'
 
+module ActiveActions::Generators ; end
+
 class ActiveActions::Generators::ActionGenerator < ::Rails::Generators::NamedBase
   source_root File.expand_path('templates', __dir__)
 
