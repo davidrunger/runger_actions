5833bcbbfe194dbe3aced8ec20a850ebc821acf8
David Runger <davidjrunger@gmail.com>
2023-05-20 13:34:43 -0500

    Prepare to release v0.19.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,4 @@
-## Unreleased
+## v0.19.0 (2023-05-20)
 ### Added
 - Release gem via RubyGems.
 
--- lib/runger_actions/version.rb
+++ lib/runger_actions/version.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 module RungerActions
-  VERSION = '0.18.1.alpha'
+  VERSION = '0.19.0'
 end
