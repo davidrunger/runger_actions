7b2e23957627474d58c908c90763c45b0d97ad36
David Runger <davidjrunger@gmail.com>
2025-02-05 06:18:38 -0600

    Remove manual require of the gem itself
    This is not needed, because we have `Bundler.require(:default, :test)`,
    and we have `gemspec` in the default group of the `Gemfile`.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -17,8 +17,6 @@ end
 require 'bundler/setup'
 Bundler.require(:default, :test)
 
-require 'runger_actions'
-
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
   config.example_status_persistence_file_path = '.rspec_status'
